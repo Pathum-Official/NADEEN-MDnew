2b3de5d55f770bf21af352d6004362085e88fd57
Pathum Official <110227514+Pathum-Official@users.noreply.github.com>
2024-12-26 11:39:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "8poygRZB#uWGX52Pfi4W7JWlpGxFG1xbekyW-qGqpmAZv08ksQ4o", //put your session id
+SESSION_ID: process.env.SESSION_ID || "wpwSCbRb#eBiB-DCNiBcmFLIRq7foObWm3rR9f4LQKrlE4D1HPTg", //put your session id
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/2nmi9q.png",
 MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
 AUTO_VOICE:"false" //true or false
